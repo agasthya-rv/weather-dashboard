3d698dc20b3088e1d0d52a82628fe7d6fd1bdac2
Rajesh Vasireddy <rvasireddy@unm.edu>
2016-05-13 21:38:38 -0400

    weather service

--- src/client/app/weather/weather.controller.js
+++ src/client/app/weather/weather.controller.js
@@ -2,6 +2,7 @@ angular
 .module("app.weather", [])
 .controller("Weather", function($scope, $routeParams, weatherSvc) {
 	$scope.current = null;
+    $scope.getTime = getTime;
 	
 	if($routeParams != undefined) {
 		getCurrent($routeParams.id);
@@ -12,11 +13,15 @@ angular
 			.then(
 				function(response) {
 					console.log(response);
-					$scope.current = response.list;
+					$scope.current = response;
 				},
 				function(err) {
 					console.log("error loading current weather data: ", err);
 				}
 		)
 	}
+    
+    function getTime(dt) {
+        return new Date(dt*1000);
+    }
 })
